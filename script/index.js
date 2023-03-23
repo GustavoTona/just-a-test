59bf46ab1b61ec397e54517e24cf3b30bd6799cd
Gustavo Toná <gustavopicolitona@gmail.com>
2023-03-23 18:04:23 -0300

    It worked :)

--- script/index.js
+++ script/index.js
@@ -1,9 +1,17 @@
-let nota1 = document.getElementById("nota1");
-let nota2 = document.getElementById("nota2");
-let nota3 = document.getElementById("nota3");
+document.getElementById("calculate").addEventListener("click", function(){
+
+    let nota1 = document.getElementById("nota1").value;
+    let nota2 = document.getElementById("nota2").value;
+    let nota3 = document.getElementById("nota3").value;
 
 
+     const total = parseInt(nota1) + parseInt(nota2) + parseInt(nota3);
 
-resultado = nota1 + nota2 + nota3;
+     if (total > 180) {
+        resultado = " APROVADO!";
+      } else {
+        resultado = " REPROVADO!";
+      }
 
-console.log(resultado);
+     document.getElementById("total").innerHTML = (resultado);
+});

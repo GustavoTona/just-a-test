96300f66d94c1b9c4c0bf61429bd2844cf8137b2
Gustavo Toná <gustavopicolitona@gmail.com>
2023-03-23 13:25:31 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,9 @@
+let nota1 = document.getElementById("nota1");
+let nota2 = document.getElementById("nota2");
+let nota3 = document.getElementById("nota3");
+
+
+
+resultado = nota1 + nota2 + nota3;
+
+console.log(resultado);
